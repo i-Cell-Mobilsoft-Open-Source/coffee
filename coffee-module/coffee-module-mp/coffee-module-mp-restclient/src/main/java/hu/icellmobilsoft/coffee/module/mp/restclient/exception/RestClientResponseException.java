2633e500526dcf3a73777d1e0a804a61baa24c47
Tamás Károly <karoly.tamas@icellmobilsoft.hu>
2021-05-27 12:42:02 +0200

    #171 - DefaultExceptionMessageTranslator kiegeszites

--- coffee-module/coffee-module-mp/coffee-module-mp-restclient/src/main/java/hu/icellmobilsoft/coffee/module/mp/restclient/exception/RestClientResponseException.java
+++ coffee-module/coffee-module-mp/coffee-module-mp-restclient/src/main/java/hu/icellmobilsoft/coffee/module/mp/restclient/exception/RestClientResponseException.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -90,13 +90,16 @@ public class RestClientResponseException extends BaseException {
         if (baseExceptionResultType == null) {
             return null;
         }
-        RestClientResponseException exception = new RestClientResponseException(FaultTypeParser.parseFaultType(baseExceptionResultType.getFaultType()), baseExceptionResultType.getMessage());
-        exception.setService(baseExceptionResultType.getService());
+
+        Throwable causedBy = null;
+        if (baseExceptionResultType.isSetCausedBy()) {
+            causedBy = fromExceptionResult(baseExceptionResultType.getCausedBy());
+        }
+
+        RestClientResponseException exception = new RestClientResponseException(
+                FaultTypeParser.parseFaultType(baseExceptionResultType.getFaultType()), baseExceptionResultType.getMessage(), causedBy);
         exception.setClassName(baseExceptionResultType.getClassName());
         exception.setException(baseExceptionResultType.getException());
-        if (baseExceptionResultType.isSetCausedBy()) {
-            exception.initCause(fromExceptionResult(baseExceptionResultType.getCausedBy()));
-        }
         return exception;
     }
 }
