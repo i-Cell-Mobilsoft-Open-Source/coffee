20671fe65dbba1e84b5d9e09e200bb291c83e44d
mate.biro <mate.biro@icellmobilsoft.hu>
2023-01-16 20:01:33 +0100

    OSP-55 - sonatype-lift fixes

--- coffee-rest/src/main/java/hu/icellmobilsoft/coffee/rest/log/BaseRestLogger.java
+++ coffee-rest/src/main/java/hu/icellmobilsoft/coffee/rest/log/BaseRestLogger.java
@@ -21,6 +21,7 @@ package hu.icellmobilsoft.coffee.rest.log;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +29,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.WriterInterceptor;
@@ -135,7 +137,7 @@ public abstract class BaseRestLogger implements ContainerRequestFilter, WriterIn
                 ResponseEntityCollectorOutputStream responseEntityCollectorOutputStream = new ResponseEntityCollectorOutputStream(originalStream);
                 context.setOutputStream(responseEntityCollectorOutputStream);
                 context.proceed();
-                entity = responseEntityCollectorOutputStream.getEntityText().getBytes();
+                entity = responseEntityCollectorOutputStream.getEntityText().getBytes(StandardCharsets.UTF_8);
             } else {
                 context.proceed();
             }
@@ -166,10 +168,11 @@ public abstract class BaseRestLogger implements ContainerRequestFilter, WriterIn
      * @see RequestResponseLogger#printRequestHeaders(java.util.Map)
      */
     protected void appendRequestHeaders(StringBuilder b, ContainerRequestContext requestContext) {
-        b.append(requestResponseLogger.printRequestHeaders(requestContext.getHeaders()));
+        MultivaluedMap<String, String> headers = requestContext.getHeaders();
+        b.append(requestResponseLogger.printRequestHeaders(headers));
         String sessionId = null;
-        if (requestContext.getHeaders().containsKey(sessionKey())) {
-            sessionId = requestContext.getHeaders().get(sessionKey()).get(0);
+        if (headers != null && headers.containsKey(sessionKey())) {
+            sessionId = headers.get(sessionKey()).get(0);
         }
         MDC.put(LogConstants.LOG_SESSION_ID, StringUtils.defaultIfBlank(sessionId, RandomUtil.generateId()));
     }
