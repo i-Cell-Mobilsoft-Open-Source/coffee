5239e61b5f46c1a87102ce873bd17c84c37f3cce
Czencz László <laciczencz@gmail.com>
2023-07-05 19:45:31 +0200

    #480 Missing microprofile-config.properties
    DefaultEtcdConfigImpl classloader fix

--- coffee-module/coffee-module-etcd/src/main/java/hu/icellmobilsoft/coffee/module/etcd/config/DefaultEtcdConfigImpl.java
+++ coffee-module/coffee-module-etcd/src/main/java/hu/icellmobilsoft/coffee/module/etcd/config/DefaultEtcdConfigImpl.java
@@ -19,9 +19,13 @@
  */
 package hu.icellmobilsoft.coffee.module.etcd.config;
 
+import java.util.Optional;
+
 import jakarta.enterprise.context.Dependent;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 
 import hu.icellmobilsoft.coffee.tool.utils.config.ConfigUtil;
 
@@ -34,16 +38,24 @@ import hu.icellmobilsoft.coffee.tool.utils.config.ConfigUtil;
 @Dependent
 public class DefaultEtcdConfigImpl implements EtcdConfig {
 
-    /**
-     * Configurationa key for ETCD URL
-     */
-    public static final String URL_KEY = "coffee.etcd.default.url";
+	/**
+	 * Configurationa key for ETCD URL
+	 */
+	public static final String URL_KEY = "coffee.etcd.default.url";
 
-    /** {@inheritDoc} */
-    @Override
-    public String[] getUrl() {
-        String urlString = ConfigUtil.getInstance().defaultConfig().getOptionalValue(URL_KEY, String.class).orElse("http://localhost:2379");
-        return StringUtils.split(urlString, ",");
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String[] getUrl() {
+		String url = "";
+		Optional<String> optUrl = ConfigUtil.getInstance().defaultConfig().getOptionalValue(URL_KEY, String.class);
+		if (optUrl.isEmpty()) {
+			Config config = ConfigProviderResolver.instance().getBuilder().forClassLoader(DefaultEtcdConfigImpl.class.getClassLoader())
+					.addDefaultSources().build();
+			url = config.getOptionalValue(URL_KEY, String.class).orElse("http://localhost:2379");
+		} else {
+			url = optUrl.get();
+		}
+		return StringUtils.split(url, ",");
+	}
 
 }
