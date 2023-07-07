edb30cb9d2b936e09ab53f8f9f670a7037310ced
Tamas Cserhati <cserhatit@gmail.com>
2023-07-07 09:04:32 +0200

    #484 HTTP status code added to RestClientResponseException on master v1

--- coffee-dto/coffee-dto-base/src/main/java/hu/icellmobilsoft/coffee/dto/exception/RestClientResponseException.java
+++ coffee-dto/coffee-dto-base/src/main/java/hu/icellmobilsoft/coffee/dto/exception/RestClientResponseException.java
@@ -33,6 +33,7 @@ public class RestClientResponseException extends BaseException {
     private String service;
     private String className;
     private String exception;
+    private int statusCode;
 
     public RestClientResponseException(String message) {
         super(CoffeeFaultType.REST_CLIENT_EXCEPTION, message);
@@ -109,5 +110,19 @@ public class RestClientResponseException extends BaseException {
      */
     public void setService(String service) {
         this.service = service;
+    }
+
+    /**
+     * @return the statusCode
+     */
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * @param statusCode the statusCode to set
+     */
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
     }
 }
