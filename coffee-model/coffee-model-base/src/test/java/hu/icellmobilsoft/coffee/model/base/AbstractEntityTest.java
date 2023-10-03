1ffa0dc60cb357945b321850b8a8a70f5d8ba83d
attila-kiss-it <attila.kiss.it@gmail.com>
2023-10-03 11:45:02 +0200

    536 AbstractEntity toString fix

---
+++ coffee-model/coffee-model-base/src/test/java/hu/icellmobilsoft/coffee/model/base/AbstractEntityTest.java
@@ -0,0 +1,49 @@
+/*-
+ * #%L
+ * Coffee
+ * %%
+ * Copyright (C) 2020 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package hu.icellmobilsoft.coffee.model.base;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing {@link AbstractEntity} class.
+ *
+ * @author attila.kiss
+ */
+@DisplayName("Testing AbstractEntity class")
+public class AbstractEntityTest {
+
+    @Test
+    @DisplayName("Testing toString method")
+    public void toStringTest() {
+        // given
+        AbstractEntity entity = new DefaultImplAbstractEntity();
+
+        // when
+
+        // then
+        String actualToStringValue = entity.toString();
+        Assertions.assertTrue(actualToStringValue.contains("blobColumn=Blob"));
+        Assertions.assertTrue(actualToStringValue.contains("byteArrayColumn=byte[]"));
+        Assertions.assertTrue(actualToStringValue.contains("clobColumn=Clob"));
+    }
+
+}
---
+++ coffee-model/coffee-model-base/src/test/java/hu/icellmobilsoft/coffee/model/base/DefaultImplAbstractEntity.java
@@ -0,0 +1,76 @@
+/*-
+ * #%L
+ * Coffee
+ * %%
+ * Copyright (C) 2020 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package hu.icellmobilsoft.coffee.model.base;
+
+import java.sql.Blob;
+import java.sql.Clob;
+
+import jakarta.enterprise.inject.Vetoed;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+/**
+ * Default implementation of {@link AbstractEntity}
+ *
+ * @author attila.kiss
+ */
+@Vetoed
+@Entity
+@Table(name = "dummy")
+public class DefaultImplAbstractEntity extends AbstractEntity {
+
+    @Column
+    private byte[] byteArrayColumn;
+
+    @Lob
+    @Column
+    private Blob blobColumn;
+
+    @Lob
+    @Column
+    private Clob clobColumn;
+
+    public byte[] getByteArrayColumn() {
+        return byteArrayColumn;
+    }
+
+    public void setByteArrayColumn(byte[] byteArrayColumn) {
+        this.byteArrayColumn = byteArrayColumn;
+    }
+
+    public Blob getBlobColumn() {
+        return blobColumn;
+    }
+
+    public void setBlobColumn(Blob blobColumn) {
+        this.blobColumn = blobColumn;
+    }
+
+    public Clob getClobColumn() {
+        return clobColumn;
+    }
+
+    public void setClobColumn(Clob clobColumn) {
+        this.clobColumn = clobColumn;
+    }
+
+}
