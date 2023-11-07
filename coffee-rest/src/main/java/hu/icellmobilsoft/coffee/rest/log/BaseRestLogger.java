b06ff6a6969e342d92bb436ce71e8cf895277b4c
mate.biro <mate.biro@icellmobilsoft.hu>
2023-11-07 09:25:49 +0100

    OSP-55 - RequestResponseLogger memoria tuning

--- coffee-rest/src/main/java/hu/icellmobilsoft/coffee/rest/log/BaseRestLogger.java
+++ coffee-rest/src/main/java/hu/icellmobilsoft/coffee/rest/log/BaseRestLogger.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -107,7 +107,7 @@ public abstract class BaseRestLogger implements ContainerRequestFilter, WriterIn
 
     /**
      * Processes HTTP response.
-     *
+     * 
      * @param context
      *            context
      * @return HTTP response message or null if logging is disabled
