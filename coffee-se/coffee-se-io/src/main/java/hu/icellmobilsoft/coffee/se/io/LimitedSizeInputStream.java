5360b056fd7a87fc1ff147cc51eabcff537bde32
attila-kiss-it <attila.kiss.it@gmail.com>
2024-07-30 11:33:32 +0200

    687 LimitedSizeInputStream in coffee se

---
+++ coffee-se/coffee-se-io/src/main/java/hu/icellmobilsoft/coffee/se/io/LimitedSizeInputStream.java
@@ -0,0 +1,102 @@
+/*-
+ * #%L
+ * Coffee
+ * %%
+ * Copyright (C) 2020 - 2024 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package hu.icellmobilsoft.coffee.se.io;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+
+import hu.icellmobilsoft.coffee.se.io.exception.SizeLimitExceededIOException;
+
+/**
+ * {@link FilterInputStream} extension that throws {@link SizeLimitExceededIOException} if the {@code maxSize} is reached while reading the stream.
+ *
+ * @author balazs.joo
+ * @author zsolt.doma
+ * @author attila-kiss-it
+ * @since 2.8.0
+ */
+public class LimitedSizeInputStream extends FilterInputStream {
+
+    private final long maxSize;
+
+    private long total;
+
+    /**
+     * Creates a {@code LimitedSizeInputStream} by assigning the argument {@code in} to the field {@code this.in} so as to remember it for later use.
+     * 
+     * @param in
+     *            the underlying input stream
+     * @param maxSize
+     *            the maximum size in bytes that is allowed to read
+     */
+    public LimitedSizeInputStream(InputStream in, long maxSize) {
+        super(in);
+        this.maxSize = maxSize;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * If {@code maxSize} is reached it throws a {@link SizeLimitExceededIOException} that can be handled separately from other {@link IOException}.
+     */
+    @Override
+    public int read() throws IOException {
+        int i = super.read();
+        if (i >= 0) {
+            incrementCounter(1);
+        }
+        return i;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * If {@code maxSize} is reached it throws a {@link SizeLimitExceededIOException} that can be handled separately from other {@link IOException}.
+     */
+    @Override
+    public int read(byte[] b) throws IOException {
+        return read(b, 0, b.length);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * If {@code maxSize} is reached it throws a {@link SizeLimitExceededIOException} that can be handled separately from other {@link IOException}.
+     */
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int i = super.read(b, off, len);
+        if (i >= 0) {
+            incrementCounter(i);
+        }
+        return i;
+    }
+
+    private void incrementCounter(int size) throws SizeLimitExceededIOException {
+        total += size;
+        if (total > maxSize) {
+            throw new SizeLimitExceededIOException(
+                    MessageFormat.format("InputStream exceeded maximum size in bytes. Read bytes [{0}], maximum size [{1}]", total, maxSize));
+        }
+    }
+
+}
---
+++ coffee-se/coffee-se-io/src/test/java/hu/icellmobilsoft/coffee/se/io/LimitedSizeInputStreamTest.java
@@ -0,0 +1,243 @@
+/*-
+ * #%L
+ * Coffee
+ * %%
+ * Copyright (C) 2020 - 2024 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package hu.icellmobilsoft.coffee.se.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import hu.icellmobilsoft.coffee.se.io.exception.SizeLimitExceededIOException;
+
+@DisplayName("Testing LimitedSizeInputStream")
+class LimitedSizeInputStreamTest {
+
+    private static interface InputStreamConsumer {
+
+        void consume(InputStream inputStream) throws IOException;
+
+    }
+
+    private final int MAX_SIZE = 5;
+
+    @Test
+    @DisplayName("Testing empty InputStream consumed by read()")
+    void testEmptyInputStreamConsumeByRead() {
+
+        // given
+        InputStream inputStream = createInputStream(0);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByRead;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing empty InputStream consumed by read(byte[] b)")
+    void testEmptyInputStreamConsumeByReadBuffer() {
+
+        // given
+        InputStream inputStream = createInputStream(0);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBuffer;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing empty InputStream consumed by read(byte b[], int off, int len)")
+    void testEmptyInputStreamConsumeByReadBufferOffset() {
+
+        // given
+        InputStream inputStream = createInputStream(0);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBufferOffset;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing InputStream consumed by read()")
+    void testInputStreamConsumeByRead() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE - 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByRead;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing InputStream consumed by read(byte[] b)")
+    void testInputStreamConsumeByReadBuffer() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE - 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBuffer;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing InputStream consumed by read(byte b[], int off, int len)")
+    void testInputStreamConsumeByReadBufferOffset() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE - 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBufferOffset;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing max size InputStream consumed by read()")
+    void testMaxSizeInputStreamConsumeByRead() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByRead;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing max size InputStream consumed by read(byte[] b)")
+    void testMaxSizeInputStreamConsumeByReadBuffer() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBuffer;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing max size InputStream consumed by read(byte b[], int off, int len)")
+    void testMaxSizeInputStreamConsumeByReadBufferOffset() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBufferOffset;
+
+        testHappyCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing too big InputStream consumed by read()")
+    void testTooBigInputStreamConsumeByRead() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE + 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByRead;
+
+        testSizeLimitExceededCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing too big InputStream consumed by read(byte[] b)")
+    void testTooBigInputStreamConsumeByReadBuffer() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE + 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBuffer;
+
+        testSizeLimitExceededCase(inputStream, inputStreamConsumer);
+    }
+
+    @Test
+    @DisplayName("Testing too big InputStream consumed by read(byte b[], int off, int len)")
+    void testTooBigInputStreamConsumeByReadBufferOffset() {
+
+        // given
+        InputStream inputStream = createInputStream(MAX_SIZE + 1);
+        InputStreamConsumer inputStreamConsumer = this::consumeInputStreamByReadBufferOffset;
+
+        testSizeLimitExceededCase(inputStream, inputStreamConsumer);
+    }
+
+    private void testHappyCase(InputStream inputStream, InputStreamConsumer inputStreamConsumer) {
+
+        try (inputStream) {
+
+            // when
+            inputStreamConsumer.consume(inputStream);
+
+            // then
+            // OK
+
+        } catch (IOException e) {
+
+            // then
+            Assertions.fail(e);
+        }
+    }
+
+    private void testSizeLimitExceededCase(InputStream inputStream, InputStreamConsumer inputStreamConsumer) {
+
+        try (inputStream) {
+
+            // when
+            inputStreamConsumer.consume(inputStream);
+
+            // then
+            Assertions.fail("Should throw SizeLimitExceededIOException");
+            
+        } catch (SizeLimitExceededIOException e) {
+
+            // then
+            Assertions.assertNotNull(e);
+
+        } catch (IOException e) {
+
+            // then
+            Assertions.fail("Should throw SizeLimitExceededIOException instead of ", e);
+        }
+    }
+
+    private LimitedSizeInputStream createInputStream(int size) {
+        return new LimitedSizeInputStream(new ByteArrayInputStream(new byte[size]), MAX_SIZE);
+    }
+
+    private void consumeInputStreamByRead(InputStream inputStream) throws IOException {
+
+        while (inputStream.read() >= 0) {
+            // nothing to do
+        }
+    }
+
+    private void consumeInputStreamByReadBuffer(InputStream inputStream) throws IOException {
+
+        byte[] buffer = new byte[2];
+
+        while (inputStream.read(buffer) >= 0) {
+            // nothing to do
+        }
+    }
+
+    private void consumeInputStreamByReadBufferOffset(InputStream inputStream) throws IOException {
+
+        byte[] buffer = new byte[2];
+
+        while (inputStream.read(buffer, 0, 2) >= 0) {
+            // nothing to do
+        }
+    }
+
+}
