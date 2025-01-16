8df5a726176dcfd4a3d1dd5309749dcb90711fa6
attila-kiss-it <attila.kiss.it@gmail.com>
2025-01-16 11:32:36 +0100

    734 AbstractIdentifiedEntity implements IIdentifiedEntity

--- coffee-model/coffee-model-base/src/main/java/hu/icellmobilsoft/coffee/model/base/AbstractIdentifiedEntity.java
+++ coffee-model/coffee-model-base/src/main/java/hu/icellmobilsoft/coffee/model/base/AbstractIdentifiedEntity.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,13 @@
  */
 package hu.icellmobilsoft.coffee.model.base;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-
-import org.hibernate.annotations.GenericGenerator;
 
 /**
  * Abstract AbstractIdentifiedEntity class.
@@ -33,7 +33,7 @@ import org.hibernate.annotations.GenericGenerator;
  * @since 1.0.0
  */
 @MappedSuperclass
-public abstract class AbstractIdentifiedEntity extends AbstractEntity {
+public abstract class AbstractIdentifiedEntity extends AbstractEntity implements IIdentifiedEntity<String> {
 
     private static final long serialVersionUID = 1L;
 
@@ -55,19 +55,21 @@ public abstract class AbstractIdentifiedEntity extends AbstractEntity {
 
     /**
      * Getter for the field {@code id}.
-     * 
+     *
      * @return id
      */
+    @Override
     public String getId() {
         return id;
     }
 
     /**
      * Setter for the field {@code id}.
-     * 
+     *
      * @param id
      *            id
      */
+    @Override
     public void setId(String id) {
         this.id = id;
     }
